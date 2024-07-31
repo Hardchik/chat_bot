4d437a5f984f635a339a450737c7721973f9afd1
adityaaverma <aditya.aryan.ug20@nsut.ac.in>
2024-07-31 10:49:30 +0100

    deleting unnessesary files

---
+++ ulbot/components/chatWindow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Message from './message';
+
+const ChatWindow = ({ messages }) => {
+  return (
+    <div className="chat-window">
+      {messages.map((message, index) => (
+        <Message key={index}   
+ message={message} />
+      ))}
+    </div>
+  );
+};
+
+export default ChatWindow;
